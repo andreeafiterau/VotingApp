7f9843c60aa6e34fd95a4c15c634abee5a9e7ab2
Fiterau Andreea <andreeafiterau@yahoo.co.uk>
2020-12-11 12:34:19 -0800

    deleted front end will make some new

--- OnlineVotingApp-Frontend/OnlineVotingApp/src/app/models/user.ts
+++
@@ -1,9 +0,0 @@
-import { ɵangular_packages_platform_browser_dynamic_platform_browser_dynamic_a } from '@angular/platform-browser-dynamic';
-
-export class User{
-
-    idUser:number;
-    username:string;
-    password:string;
-    idApplicationRole:number;
-}
